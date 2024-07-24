680832b3bb7be519aa04144d81404462e17459df
girirajsg <148320526+girirajsg@users.noreply.github.com>
2024-07-24 13:46:01 +0530

    Add files via upload

---
+++ drive.js
@@ -0,0 +1,20 @@
+let bt=document.getElementById("#btn");
+btn.onclick=()=>{
+    alert("Please Register to Book");
+}
+
+let myButton=document.querySelector("#bk1");
+myButton.onclick=()=>{
+    console.log("Your Vehicle is on the way");
+}
+
+
+let myButton2=document.querySelector("#bk2");
+myButton2.onclick=()=>{
+    console.log("Your Vehicle is on the way");
+}
+
+let myButton3=document.querySelector("#bk3");
+myButton3.onclick=()=>{
+    alert("Your Vehicle is on the way kindly wait");
+}
